a39833c21753f5256be4a486cb8a005a34862794
lilpsh <pashadesnov@gmail.com>
2025-05-24 15:46:59 +0300

    headless true

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
     use: {
         baseURL: 'https://opensource-demo.orangehrmlive.com',
-        headless: false, // <- увімкнено відображення
+        headless: true, // відображення
         screenshot: 'only-on-failure',
         video: 'retain-on-failure', // можна додати запис відео
         trace: 'on-first-retry',
